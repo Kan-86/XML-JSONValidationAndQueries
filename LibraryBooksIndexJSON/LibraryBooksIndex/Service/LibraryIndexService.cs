2a12ecb2301bb0e5349a35c43e4b42b68541ac26
Kristofer Andersen <andersen19@gmail.com>
2020-05-29 15:30:27 +0200

    some refactoring
    added few if statements incase of null value

--- LibraryBooksIndexJSON/LibraryBooksIndex/Service/LibraryIndexService.cs
+++ LibraryBooksIndexJSON/LibraryBooksIndex/Service/LibraryIndexService.cs
@@ -14,22 +14,31 @@ namespace LibraryBooksIndex.Service
 {
     public class LibraryIndexService
     {
-
         string jsonSchema = File.ReadAllText(@"W:\Skoli\6onn\DBDFinalExam\XML-JSONValidationAndQueries\BooksAndUsersXMLJSON\LibraryJSONSchema.json");
         string jsonFile = File.ReadAllText(@"W:\Skoli\6onn\DBDFinalExam\XML-JSONValidationAndQueries\BooksAndUsersXMLJSON\LibraryMockJSON.json");
         public void AveragePriceOfBooks(string name)
         {
             var userName = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Users>>(jsonFile);
 
-            var peopleOverForty = from p in userName
+            var averagePrice = from p in userName
                                   .Where (p => p.Name == name )
                                   .SelectMany(r => r.BooksRented)
                                   .Select(a => a.Price)
                                   select p;
 
 
-            double result = peopleOverForty.Average();
-            Console.WriteLine($"Average price for this user is: {result}");
+            
+
+            if (averagePrice.Count() != 0)
+            {
+                double result = averagePrice.Average();
+                Console.WriteLine($"Average price for this user is: {result}");
+            }
+            else
+            {
+                Console.WriteLine($"No record found by the username {name}, please verify and try again.");
+            }
+
             Console.ReadLine();
         }
 
@@ -37,17 +46,24 @@ namespace LibraryBooksIndex.Service
         {
             var userName = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Users>>(jsonFile);
 
-            var peopleOverForty = from p in userName
+            var titlesAbovePrice = from p in userName
                                   .SelectMany(p => p.BooksRented)
-                                  .Where(r => r.Price < price)
+                                  .Where(r => r.Price > price)
                                   .Select(n => n.Title)
                                   select p.ToString();
 
-            Console.WriteLine($"Titles that cost more than {price} are: \n");
-
-            foreach (var item in peopleOverForty)
+            if (titlesAbovePrice.Count() != 0)
             {
-                Console.WriteLine($"Title: {item}");
+                Console.WriteLine($"Titles that cost more than {price} are: \n");
+
+                foreach (var item in titlesAbovePrice)
+                {
+                    Console.WriteLine($"Title: {item}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"No title is above {price}kr price, try a different price.");
             }
             Console.ReadLine();
         }
@@ -67,13 +83,18 @@ namespace LibraryBooksIndex.Service
                 user.UserId = usr.UserId;
                 user.Name = usr.Name;
                 user.Address = usr.Address;
-
             }
-
-            Console.WriteLine($"Results: \n " +
-                                $"\t Id: {user.UserId} \n" +
-                                $"\t Username: {user.Name} \n" +
-                                $"\t Address: {user.Address}");
+            if (user.UserId != 0)
+            {
+                Console.WriteLine($"Results: \n " +
+                    $"\t Id: {user.UserId} \n" +
+                    $"\t Username: {user.Name} \n" +
+                    $"\t Address: {user.Address}");
+            }
+            else
+            {
+                Console.WriteLine("No data found, please try again.");
+            }
 
             Console.ReadLine();
         }
